865c11dd62ff4ef7b057175f9334618c50a158a1
joseCarlosAc <jose.acevesgon@gmail.com>
2023-07-12 13:46:01 -0600

    nodos

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const padre = document.getElementById("padre");
 const padre2 = document.getElementById("padre2");
 const parrafo = document.getElementById("parrafo");
-function crearNodos() {}
+function crearNodos() {
+	let nodeTemp = document.createElement("p");
+	padre.append(nodeTemp);
+}
