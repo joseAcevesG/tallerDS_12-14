641ff33135699435b17ccc13262c63d7dfaff0d2
ACEVES GONZALEZ <jose.aceves@iteso.mx>
2023-07-12 13:37:24 -0500

    third commit

--- script.js
+++ script.js
@@ -5,5 +5,13 @@ const boton = document.getElementById("boton");
 function clickBoton() {
 	if (parrafo.innerHTML == "test") {
 		parrafo.innerHTML = "Hola";
-	}
+	}else if(boton.getAttribute("estado") == "noClick"){
+        parrafo.innerHTML = "Mundo";
+        boton.setAttribute("estado", "Click");
+    }else if(parrafo.innerHTML == "Mundo" && boton.getAttribute("estado") == "Click" ){
+        parrafo.innerHTML = "Taller IDS";
+    }else{
+        parrafo.innerHTML = "test";
+        boton.setAttribute("estado", "noClick");
+    }
 }
