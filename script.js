8f783dc780b105d5138814ae3eb97f2b26aef34a
joseCarlosAc <jose.acevesgon@gmail.com>
2023-07-12 13:56:17 -0600

    Dejercicio 2

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const padre = document.getElementById("padre");
 const padre2 = document.getElementById("padre2");
 const parrafo = document.getElementById("parrafo");
+
+let rectangulo = new Rectangulo();
+
 function crearNodos() {
 	let nodeTemp = document.createElement("p");
 	padre.append(nodeTemp);
 }
+
+function listaComida() {}
+
+function quitarNodo() {}
+
+function Rectangulo() {}
