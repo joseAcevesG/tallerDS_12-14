b87c3d5265b7163d819e6625eecab85c9b0012cd
joseCarlosAc <jose.acevesgon@gmail.com>
2023-07-12 13:39:45 -0600

    ejercicio dos

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
+const padre = document.getElementById("padre");
+const padre2 = document.getElementById("padre2");
 const parrafo = document.getElementById("parrafo");
-
-const boton = document.getElementById("boton");
-
-function clickBoton() {
-	if (parrafo.innerHTML == "test") {
-		parrafo.innerHTML = "Hola";
-	}else if(boton.getAttribute("estado") == "noClick"){
-        parrafo.innerHTML = "Mundo";
-        boton.setAttribute("estado", "Click");
-    }else if(parrafo.innerHTML == "Mundo" && boton.getAttribute("estado") == "Click" ){
-        parrafo.innerHTML = "Taller IDS";
-    }else{
-        parrafo.innerHTML = "test";
-        boton.setAttribute("estado", "noClick");
-    }
-}
-
-function botenes(boton) {
-	const parrafo = document.getElementById("parrafo2");
-	let numero = Number(boton.getAttribute("numero"));
-
-	switch (numero) {
-	}
-}
+function crearNodos() {}
